1960995b7cdfa4c07b905a66284e9a2bfdafca19
Xiao-Ping Huynh <huynhxiaoping@gmail.com>
2018-11-08 16:48:43 -0500

    bare min dynamic table

--- dynamic_table.js
+++ dynamic_table.js
@@ -11,8 +11,7 @@ function grabJSON(filepath) {
     Httpreq.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             json_obj = JSON.parse(Httpreq.responseText);
-            // debug
-            console.log(json_obj);
+            // console.log(json_obj);
             makeTableFromJSON(json_obj);
         } else if (this.status == 404) {
             alert("JSON file not found, recheck the filepath :(");
@@ -23,9 +22,14 @@ function grabJSON(filepath) {
 function makeTableFromJSON(json_obj) {
 
     // grab the table display element and set up for the table
-    var table_display = document.getElementsByTagName("table_display");
+    var table_display = document.getElementById("dynamic_table");
     var table = document.createElement("table");
     var table_body = document.createElement("tbody");
+
+    // cleaning the old table is part of setup
+    while(table_display.firstChild) {
+        table_display.removeChild(table_display.firstChild);
+    }
 
     // get the number of rows and columns we have
     var rows = json_obj.length;
@@ -34,6 +38,30 @@ function makeTableFromJSON(json_obj) {
     }
     // var cols = json_obj[0].length; // incorrect
     var cols = Object.keys(json_obj[0]).length;
-    // debug
-    console.log("ROWS: " + rows + ", COLS: " + cols);
+    // console.log("DEBUG -> ROWS: " + rows + ", COLS: " + cols);
+
+    // start some variables outside the loops and reuse them
+    var table_row = table_body.insertRow(0); 
+    var table_col;
+    var json_keys = Object.keys(json_obj[0]);
+    // console.log(json_keys);
+
+    // build the header row
+    var i = 0;
+    json_keys.forEach(function(key) {
+        table_col = table_row.insertCell(i);
+        table_col.innerHTML = key;
+        i++;
+    })
+    
+    for (var i=0; i<rows; i++) {
+        table_row = table_body.insertRow(i+1);
+        for (var j=0; j<cols; j++) {
+            table_col = table_row.insertCell(j);
+            table_col.innerHTML = json_obj[i][json_keys[j]];
+        }
+    }
+
+    table.appendChild(table_body);
+    table_display.appendChild(table);
 }
