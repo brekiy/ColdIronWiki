37ae7854874e910f6e5960fcd2f0112fc8c78989
Xiao-Ping Huynh <huynhxiaoping@gmail.com>
2018-12-11 03:07:24 -0500

    added 2 more creatures, still haven't put any on the bestiary index yet

---
+++ bestiary/creature_list_gen.py
@@ -0,0 +1,4 @@
+# todo:
+# grab all filenames in the json folder, strip the json
+# sort em alphabetically
+# for each creature name, enclose it in the html boilerplate and write this line to a file
